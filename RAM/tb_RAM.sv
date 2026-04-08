99c4e084094922f33440222bd889195a2f2a5466
Aspera <sciabole@icloud.com>
2026-04-08 23:52:27 +0900

    add cg

--- RAM/tb_RAM.sv
+++ RAM/tb_RAM.sv
@@ -1,7 +1,7 @@
 `include "uvm_macros.svh"
 import uvm_pkg::*;
 
-
+// === interface
 interface ram_if (
     input logic clk
 );
@@ -11,13 +11,15 @@ interface ram_if (
     logic [15:0] rdata;
 endinterface  //ram_if
 
-
+// === sequence item
 class ram_seq_item extends uvm_sequence_item;
     rand bit        we;
     rand bit [ 7:0] addr;
     rand bit [15:0] wdata;
     logic    [15:0] rdata;
 
+    // -- Constraint 
+    /*
     constraint c_we {
         we dist {
             1 := 1,
@@ -26,6 +28,7 @@ class ram_seq_item extends uvm_sequence_item;
     }
     constraint c_addr {addr inside {[0 : 9]};}
     // constraint c_wdata
+    */
 
     `uvm_object_utils_begin(ram_seq_item)
         `uvm_field_int(we, UVM_ALL_ON)
@@ -38,7 +41,7 @@ class ram_seq_item extends uvm_sequence_item;
     endfunction  //new()
 endclass  //ram_seq_item extends uvm_sequence_item
 
-
+// === sequence
 class ram_seq extends uvm_sequence #(ram_seq_item);
     `uvm_object_utils(ram_seq)
 
@@ -48,7 +51,7 @@ class ram_seq extends uvm_sequence #(ram_seq_item);
 
     virtual task body();
         ram_seq_item item;
-        repeat (100) begin
+        repeat (1000) begin
             item = ram_seq_item::type_id::create("item");
             start_item(item);
             assert (item.randomize());
@@ -57,7 +60,7 @@ class ram_seq extends uvm_sequence #(ram_seq_item);
     endtask  //body
 endclass  //ram_seq extends uvm_sequence
 
-
+// === driver
 class ram_driver extends uvm_driver #(ram_seq_item);
     `uvm_component_utils(ram_driver)
 
@@ -87,7 +90,7 @@ class ram_driver extends uvm_driver #(ram_seq_item);
     endtask  //run_phase
 endclass  //ram_driver extends uvm_driver
 
-
+// === monitor
 class ram_monitor extends uvm_monitor;
     `uvm_component_utils(ram_monitor)
 
@@ -117,13 +120,67 @@ class ram_monitor extends uvm_monitor;
             r_seq_item.addr  = r_if.addr;
             r_seq_item.wdata = r_if.wdata;
             r_seq_item.rdata = r_if.rdata;
-            `uvm_info(get_type_name(), "sampled RAM transaction", UVM_HIGH);
+            `uvm_info(get_type_name(), $sformatf(
+                      "sampled RAM transaction addr: %0h, wdata: %0h, rdata: %0h",
+                      r_if.addr,
+                      r_if.wdata,
+                      r_if.rdata
+                      ), UVM_HIGH);
             send.write(r_seq_item);
         end
     endtask  //run_phase
 endclass  //ram_monitor extends uvm_monitor
 
+// === subscriber
+class ram_coverage extends uvm_subscriber #(ram_seq_item);
+    `uvm_component_utils(ram_coverage)
 
+    ram_seq_item item;
+
+    covergroup ram_cg;
+        cp_we: coverpoint item.we {bins we_0 = {0}; bins we_1 = {1};}
+        cp_addr: coverpoint item.addr {
+            bins low = {[0 : 63]};
+            bins mid1 = {[64 : 127]};
+            bins mid2 = {[128 : 191]};
+            bins high = {[192 : 255]};
+        }
+        cp_wdata: coverpoint item.wdata;
+        cx_we_addr: cross cp_we, cp_addr;
+    endgroup
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+        ram_cg = new();
+    endfunction  //new()
+
+    virtual function void write(ram_seq_item t);
+        item = t;
+        ram_cg.sample();
+        // `uvm_info(get_type_name(), $sformatf("counter_cg sampled: %s", item.convert2string()), UVM_MEDIUM)
+    endfunction
+
+    virtual function void report_phase(uvm_phase phase);
+        `uvm_info(get_type_name(), "==== RAM Coverage Summary ====", UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf(
+                  "cp_we       : %0.2f%%", ram_cg.cp_we.get_coverage()),
+                  UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf(
+                  "cp_addr     : %0.2f%%", ram_cg.cp_addr.get_coverage()),
+                  UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf(
+                  "cp_wdata    : %0.2f%%", ram_cg.cp_wdata.get_coverage()),
+                  UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf(
+                  "cx_we_addr  : %0.2f%%", ram_cg.cx_we_addr.get_coverage()),
+                  UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf(
+                  "total       : %0.2f%%", ram_cg.get_coverage()), UVM_LOW)
+    endfunction
+endclass  //ram_coverage extends uvm_subscriber
+
+
+// === scoreboard
 class ram_scb extends uvm_scoreboard;
     `uvm_component_utils(ram_scb)
 
@@ -162,7 +219,7 @@ class ram_scb extends uvm_scoreboard;
     endfunction
 endclass  //ram_scb extends uvm_scoreboard
 
-
+// === agent
 class ram_agent extends uvm_agent;
     `uvm_component_utils(ram_agent)
 
@@ -187,12 +244,13 @@ class ram_agent extends uvm_agent;
     endfunction
 endclass  //ram_agent extends uvm_agent
 
-
+// === environment
 class ram_env extends uvm_env;
     `uvm_component_utils(ram_env)
 
     ram_agent agt;
-    ram_scb   scb;
+    ram_scb scb;
+    ram_coverage sbr;
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
@@ -202,15 +260,17 @@ class ram_env extends uvm_env;
         super.build_phase(phase);
         agt = ram_agent::type_id::create("agt", this);
         scb = ram_scb::type_id::create("scb", this);
+        sbr = ram_coverage::type_id::create("sbr", this);
     endfunction
 
     virtual function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
         agt.mon.send.connect(scb.recv);
+        agt.mon.send.connect(sbr.analysis_export);
     endfunction
 endclass  //ram_env extends uvm_env
 
-
+// === test
 class ram_test extends uvm_test;
     `uvm_component_utils(ram_test)
 
@@ -241,7 +301,7 @@ class ram_test extends uvm_test;
     endfunction
 endclass  //ram_test extends uvm_test
 
-
+// === DUT
 module tb_ram ();
     logic clk;
 
